ed8e92de5f6c5f11021fe1244b360882c9695454
TrevisGreen <Trevis.Green@gmail.com>
2014-06-07 01:07:21 -0500

    Admin User Management

--- src/main/java/org/trevisgreen/test/web/UserController.java
+++ src/main/java/org/trevisgreen/test/web/UserController.java
@@ -21,13 +21,100 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package org.trevisgreen.test.web;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.trevisgreen.test.model.User;
+import org.trevisgreen.test.service.UserService;
 
 /**
  *
  * @author Trevis
  */
-public class UserController {
-    
+@Controller
+@RequestMapping("/admin/user")
+public class UserController extends BaseController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/show/{userId}", method = RequestMethod.GET)
+    public String show(@PathVariable Long userId, Model model) {
+        User user = userService.get(userId);
+        model.addAttribute("user", user);
+        return "user/show";
+    }
+
+    @RequestMapping(value = "/delete/{userId}", method = RequestMethod.GET)
+    public String delete(@PathVariable Long userId, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            String username = userService.delete(userId);
+            redirectAttributes.addFlashAttribute("successMessage", "user.deleted");
+            redirectAttributes.addFlashAttribute("successMessageAttrs", username);
+        } catch (Exception e) {
+            log.error("Could not delete user", e);
+            redirectAttributes.addFlashAttribute("errorMessage", "user.not.deleted");
+        }
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping(value = {"", "/list"}, method = RequestMethod.GET)
+    public String list(Model model,
+            Principal principal,
+            @RequestParam(required = false) Boolean mine,
+            @RequestParam(required = false) Integer max,
+            @RequestParam(required = false) Integer offset,
+            @RequestParam(required = false) String filter,
+            @RequestParam(required = false) Long page,
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false) String order) {
+
+        Map<String, Object> params = new HashMap<>();
+        if (max != null) {
+            params.put("max", max);
+            if (offset != null) {
+                params.put("offset", offset);
+            }
+        } else {
+            params.put("max", 10);
+            params.put("offset", 0);
+        }
+
+        if (StringUtils.isNotBlank(filter)) {
+            params.put("filter", filter);
+        }
+
+        if (StringUtils.isNotBlank(sort)) {
+            params.put("sort", sort);
+        } else {
+            params.put("sort", "username");
+        }
+
+        if (StringUtils.isNotBlank(order)) {
+            params.put("order", order);
+        } else {
+            params.put("order", "asc");
+        }
+        params.put("order2", params.get("order"));
+
+        params = userService.list(params);
+
+        this.paginate(params, model, page);
+
+        model.addAllAttributes(params);
+
+        return "user/list";
+    }
+
 }
